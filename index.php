2b6d0a09d575a78503ecdc722e90c4a0399a9b13
Nguyen Van Hiep <hiep12092001@gmail.com>
2019-07-12 08:53:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Javascript unpacker testpage</title>
-		<meta name="description" content="Unpacker for JavaScript files that were packed with Dean Edwards JavaScript's Packer.">
-		<meta name="author" content="Recep Karadas">
+		
 	</head>
 	<body>
 		<h1>Javascript unpacker testpage</h1>
@@ -17,11 +16,12 @@
 			<div style="padding:10px;background:#efefef;display:inline-block;">
 			<?php
 				if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-					$jsu = new JavaScriptUnpacker();
-			?>
+					$unpacker = new JavaScriptUnpacker();
+			?>  
+ 
 				<h4>Result: </h4>
 			<?php
-					echo $jsu->Unpack($_POST["userInput"]);
+					echo $unpacker->Unpack($_POST['userInput']);
 				}else{
 			?>			
 				<label for="userInput">Put in your packed javascript:</label><br />
